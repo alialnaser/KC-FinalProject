4f96ba45b4d164346cafe6b69b52a47e94d1b7f8
alialnaser <alialnaser1233@gmail.com>
2022-09-02 12:34:03 +0300

    did some stuff

--- mc.js
+++ mc.js
@@ -1,2 +1,44 @@
 console.log("minecraft");
-alert("This Is The MineCraft Page")
+
+document.getElementById('5star')
+
+document.getElementById('4star')
+
+document.getElementById('3star')
+
+document.getElementById('2star')
+
+document.getElementById('1star')
+
+function star5() {
+    document.getElementById('5star').style.backgroundColor = 'yellow';
+}
+
+function star4() {
+    document.getElementById('4star').style.backgroundColor = 'yellow';
+}
+
+function star3() {
+    document.getElementById('3star').style.backgroundColor = 'yellow';
+}
+
+function star2() {
+    document.getElementById('2star').style.backgroundColor = 'yellow';
+}
+
+function star1() {
+    document.getElementById('1star').style.backgroundColor = 'yellow';
+}
+
+
+document.getElementById('like')
+
+document.getElementById('dislike')
+
+function like() {
+    document.getElementById('like').style.backgroundColor = 'green';
+}
+
+function dislike() {
+    document.getElementById('dislike').style.backgroundColor = 'red';
+}
